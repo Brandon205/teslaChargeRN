a178e11d9d561bdaacdae21b870a172b56448c03
brandonblack <brandonblack02@gmail.com>
2022-06-22 20:17:51 -0700

    Starting setup

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const baseURL = 'http://localhost:8080'
+  const [carData, setCarData] = useState('');
+
+  useEffect(() => {
+    axios.get(baseURL + '/vehicle/' + '1492931281739069').then(response => {
+      console.log(response.data)
+      setCarData(response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [])
+
+    axios.get(baseURL + '/home').then((response) => {
+      console.log(response);
+    }).catch(error => {
+      console.log("ERROR", error)
+    })
+
+    let content;
+    if (carData) {
+      content = (
+        <View>
+          <Text>Car Name: {carData.display_name}</Text>
+        </View>
+      )
+    }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {content}
     </View>
   );
 }
@@ -14,7 +41,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
